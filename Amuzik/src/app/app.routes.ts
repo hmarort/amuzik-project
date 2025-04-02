60cbac72f593d38330d637a92fed091e27481c9f
hmarort <hecmaror04@gmail.com>
2025-04-02 12:45:33 +0200

    Agregamos las páginas principales

--- Amuzik/src/app/app.routes.ts
+++ Amuzik/src/app/app.routes.ts
@@ -3,11 +3,31 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signin',
+    loadComponent: () => import('./pages/signin/signin.page').then( m => m.SigninPage)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'album',
+    loadComponent: () => import('./pages/album/album.page').then( m => m.AlbumPage)
+  },
+  {
+    path: 'conf',
+    loadComponent: () => import('./pages/conf/conf.page').then( m => m.ConfPage)
   },
 ];
