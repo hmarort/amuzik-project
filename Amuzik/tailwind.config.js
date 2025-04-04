0ad2e39fd4ff2a8c3f21b82a44397f7d656a4f73
hmarort <hecmaror04@gmail.com>
2025-04-04 10:25:59 +0200

    Modificado tema de la aplicación

--- Amuzik/tailwind.config.js
+++ Amuzik/tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [
+    './src/**/*.{html,ts,js}',  // Asegura que escanea tus archivos de Ionic/Angular
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ionic: {
+          primary: 'var(--ion-color-primary)',
+          secondary: 'var(--ion-color-secondary)',
+          tertiary: 'var(--ion-color-tertiary)',
+          success: 'var(--ion-color-success)',
+          warning: 'var(--ion-color-warning)',
+          danger: 'var(--ion-color-danger)',
+          light: 'var(--ion-color-light)',
+          medium: 'var(--ion-color-medium)',
+          dark: 'var(--ion-color-dark)',
+        },
+      },
+    },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')
-,require('@tailwindcss/forms')
-,require('@tailwindcss/line-clamp')
-,require('@tailwindcss/typography')
-],
-};
+  plugins: [],
+}
+
